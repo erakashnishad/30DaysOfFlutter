83b769363e288e3535242c193046a357baae949f
Akash Nishad <officialakash.gkp@gmail.com>
2021-12-24 10:56:18 +0530

    day3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   
   final int days = 10;
-  final String name = "Akash Nishad";
+  final String name = "Akash";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Demo App"),
+        title: Text("Catalog App"),
       ),
       body: Center(
         child: Container(
